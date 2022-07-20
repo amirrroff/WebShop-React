d68a1fe813581c39e891c457871abc04c1ae7221
amir <amirrroff@gmail.com>
2022-07-20 21:15:43 +0200

    resNav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,16 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image';
 import { MdShoppingBag } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
 
 function Navbar() {
+  const [showNav, setShowNav] = useState(false)
+
+  const handleShowNav= () =>{
+    setShowNav(!showNav)
+    
+  }
   return (
     <div>
       <nav className="bg-gray-800">
@@ -11,7 +18,7 @@ function Navbar() {
     <div className="relative flex items-center justify-between h-16">
       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
         {/* <!-- Mobile menu button--> */}
-        <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+        {showNav ? <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false" onClick={handleShowNav}><IoClose /></button> : <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false" onClick={handleShowNav}>
           <span className="sr-only">Open main menu</span>
           {/*<!--
             Icon when menu is closed.
@@ -33,11 +40,11 @@ function Navbar() {
           <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor" aria-hidden="true">
             <path  d="M6 18L18 6M6 6l12 12" />
           </svg>
-        </button>
+        </button> }
       </div>
       <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
         <div className="flex-shrink-0 flex items-center">
-          <Image src='/Logo.png' width={128} height={77} alt="Image" />
+          
         </div>
         <div className="hidden sm:block sm:ml-6">
           <div className="flex space-x-4">
@@ -88,13 +95,18 @@ function Navbar() {
   </div>
 
   {/*<!-- Mobile menu, show/hide based on menu state. -->*/}
+  {showNav ?  
   <div className="sm:hidden" id="mobile-menu">
-    <div className="px-2 pt-2 pb-3 space-y-1">
-        <Link href="/"><a className="  text-white hover:bg-gray-700 px-3 py-2 rounded-md text-base font-medium block" aria-current="page">Home</a></Link>
-        <Link href="/Contact"><a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 block rounded-md text-base  font-medium">Contact</a></Link>
-        <Link href="/shop"><a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 block rounded-md text-base font-medium">Shop</a></Link>
-    </div>
+  <div className="px-2 pt-2 pb-3 space-y-1">
+      <Link href="/"><a className="  text-white hover:bg-gray-700 px-3 py-2 rounded-md text-base font-medium block" aria-current="page" onClick={()=>setShowNav(false)}>Home</a></Link>
+      <Link href="/Contact"><a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 block rounded-md text-base  font-medium" onClick={()=>setShowNav(false)}>Contact</a></Link>
+      <Link href="/shop"><a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 block rounded-md text-base font-medium" onClick={()=>setShowNav(false)}>Shop</a></Link>
   </div>
+</div> 
+:
+""
+}
+ 
   
 </nav>
     </div>
